847e5d0a3b9970f8e023d3144c902b990ba0d755
Ethan-Shum <ethanshum83@gmail.com>
2026-01-22 20:19:37 +0800

    Level-1

--- src/Kiko.java
+++ src/Kiko.java
@@ -1,11 +1,25 @@
+import java.util.Scanner;
+
 public class Kiko {
     public static void main(String[] args) {
         String greeting = "____________________________________________________________\n"
-                + " Hello! I'm Kiko\n"
+                + " Hello! I'm Kiko the bunny\n"
                 + " What can I do for you?\n"
-                + "____________________________________________________________\n"
-                + " Bye. Hope to see you again soon!\n"
                 + "____________________________________________________________";
         System.out.println(greeting);
+
+        Scanner scanner = new Scanner(System.in);
+        String input;
+        
+        do {
+            input = scanner.nextLine();
+            System.out.println("____________________________________________________________");
+            System.out.println(" " + input);
+            System.out.println("____________________________________________________________");
+        } while (!input.equalsIgnoreCase("bye"));
+
+        String farewell = " Bye. Hope to see you again soon!\n"
+                + "____________________________________________________________";
+        System.out.println(farewell);
     }
 }
