8343556e2e63a9c1c42b01d6fbdd3ebf813e4ac8
Ethan-Shum <ethanshum83@gmail.com>
2026-01-22 19:55:37 +0800

    Level-0

---
+++ src/Kiko.java
@@ -0,0 +1,11 @@
+public class Kiko {
+    public static void main(String[] args) {
+        String greeting = "____________________________________________________________\n"
+                + " Hello! I'm Kiko\n"
+                + " What can I do for you?\n"
+                + "____________________________________________________________\n"
+                + " Bye. Hope to see you again soon!\n"
+                + "____________________________________________________________";
+        System.out.println(greeting);
+    }
+}
